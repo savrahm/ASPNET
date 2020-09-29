937f9eb37dc8a746272ee4fc7f6a392998979f88
Sarah <sarah.b.avram@gmail.com>
2020-09-29 19:54:18 -0400

    more product methods

---
+++ Testing/Models/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM products WHERE ProductID = @id",
+                new { id = id });
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE productID = @id",
+                new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
+
+    }
+}
